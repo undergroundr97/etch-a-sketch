d715f2ef272b04ee3feb7cb74caf19fa38d791bd
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2024-01-28 07:19:34 -0300

    fixed grid

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,27 @@
 const container = document.querySelector('.container')
 
 function makeDivs(num){
-    let box = num*num
-    for(let i = 0; i < box; i++){
+    let gridCell = ((600/num) - 2).toFixed(2);
+    let box = num*num;
+
+    while (box > 0) {
         let cells = document.createElement('div')
-        cells.setAttribute('class', 'cells')
+        cells.setAttribute('class', 'cells');
+        cells.style.height = gridCell + 'px';
+        cells.style.width = gridCell + 'px';
+        cells.style.border = '1px solid black';
         container.appendChild(cells);
+        box--;
     }
 }
 
-makeDivs(15);
+makeDivs(16);
 
-const divSelector = document.querySelectorAll('.cells')
-divSelector.addEventListener('mouseenter', () => {
-    divSelector.setAttribute('class', 'cellshover')
+
+
+let divSelector = document.querySelectorAll('.cells')
+for (let i = 0; i < divSelector.length; i++){
+divSelector[i].addEventListener('mouseover', function(e){
+  e.target.setAttribute('class', 'cells1');
 });
+}
